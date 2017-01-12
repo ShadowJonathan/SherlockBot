ff5ad96aa351b33386122ba2117c64ae8c15d2c9
ShadowJonathan <jonathandejong02@gmail.com>
2017-01-12 23:06:21 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# blue-belt-bot
+Universal Discord bot written in GoLang
+
+**Note: Blue Belt Bot is still in a experimental stage, concider helping the project or using another bot for your administrative needs.**
+
+Install git and go before downloading and compiling this bot, and run `go get github.com/bwmarrin/discordgo` in any command processor.
+
+After that, build the bot with `go build` within the `/bot` folder, and run the executable program from the same cmd.
+(note, the OsX operating system creates a file called "bot" which is not directly runable from the same folder, you have to use `../bot/bot` to run it)
---
+++ belter/belt.go
@@ -0,0 +1,70 @@
+package Belt
+
+import (
+	fmt "fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type Version struct {
+	Major               byte
+	Minor               byte
+	Build               byte
+	Experimental        bool
+	ExperimentalVersion byte
+}
+
+type Sherlock struct {
+	dg      *discordgo.Session
+	Debug   bool
+	version Version
+	OwnID   string
+	OwnAV   string
+	OwnName string
+	Stop    bool
+}
+
+// Vars after this
+
+var sh *Sherlock
+
+// Functions after this
+
+func BBReady(s *discordgo.Session, r *discordgo.Ready) {
+	sh.OwnID = r.User.ID
+	sh.OwnAV = r.User.Avatar
+	sh.OwnName = r.User.Username
+	fmt.Println("Discord: Ready message received\nBB: I am '" + sh.OwnName + "'!\nBB: My User ID: " + sh.OwnID)
+}
+
+func Initialize(Token string) {
+	isdebug, err := ioutil.ReadFile("debugtoggle")
+	sh = &Sherlock{
+		version: Version{0, 1, 0, false, 0},
+		Debug:   (err == nil && len(isdebug) > 0),
+		Stop:    false,
+	}
+	sh.dg, err = discordgo.New(Token)
+	if err != nil {
+		fmt.Println("Discord Session error, check token, error message: " + err.Error())
+		return
+	}
+	// handlers
+	sh.dg.AddHandler(BBReady)
+
+	fmt.Println("BB: Handlers installed")
+
+	err = sh.dg.Open()
+	if err == nil {
+		fmt.Println("Discord: Connection established")
+		for !sh.Stop {
+			time.Sleep(400 * time.Millisecond)
+		}
+	} else {
+		fmt.Println("Error opening websocket connection: ", err.Error())
+	}
+	fmt.Println("BBB: Blue belt stopping...")
+	sh.dg.Close()
+}
