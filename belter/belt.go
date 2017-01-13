67558d6395bd8fe47274be64778a79a7b80e3851
ShadowJonathan <jonathandejong02@gmail.com>
2017-01-13 16:10:20 +0100

    partial bot feature >.>

--- belter/belt.go
+++ belter/belt.go
@@ -1,8 +1,11 @@
 package Belt
 
 import (
+	//"bytes"
+	"encoding/json"
 	fmt "fmt"
 	"io/ioutil"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -32,12 +35,233 @@ var sh *Sherlock
 
 // Functions after this
 
+var PrimeGuild int
+var LastCheck *LastChangeStatus
+var Changes map[string]map[string]string
+
+func CheckChange(G *GuildInfo, GID string) *discordgo.Guild {
+	ChGl, err := sh.dg.State.Guild(GID)
+	if err != nil {
+		fmt.Println("Error getting guild: " + err.Error())
+	}
+	if ChGl.Channels != G.g.Channels {
+		LastCheck.Changeyes = true
+		LastCheck.Change.channel = true
+	}
+	if ChGl.Members != G.g.Members || ChGl.MemberCount != G.g.MemberCount {
+		LastCheck.Changeyes = true
+		LastCheck.Change.members = true
+	}
+	if ChGl.Roles != G.g.Roles {
+		LastCheck.Changeyes = true
+		LastCheck.Change.roles = true
+	}
+	if ChGl.Presences != G.g.Presences {
+		LastCheck.Change.yes = true
+		LastCheck.Change.presence = true
+	}
+	if ChGl.Icon != G.g.Icon || ChGl.Large != G.g.Large || ChGl.Name != G.g.Name || ChGl.OwnerID != G.g.OwnerID || ChGl.Region != G.g.Region {
+		LastCheck.Changeyes = true
+		LastCheck.Change.beginvars = true
+	}
+}
+
+// compare vals here
+
+func CompareChannel(Last *discordgo.Guild, New *discordgo.Guild) (bool, Changes) {
+	var ChS *Changes
+	var Ident bool
+	Ident = false
+	if Last.ID == New.ID {
+		Ident = true
+		if Last.Name != New.Name {
+
+		}
+	}
+	return
+}
+
+// rest funcs
+
+func AppendChange(G *discordgo.Guild) []string {
+	Resume := LastCheck.Firstcheck
+	var ChangeString []string
+	var NewString string
+	if LastCheck.Change.channel == true {
+		LastChannels := LastCheck.GI.g.Channels
+		NewChannels := G.Channels
+		if len(LastChannels) != len(NewChannels) {
+			// code for "new channel" here >.>
+		} else {
+			for _, LCh := range LastChannels {
+				for _, NCh := range NewChannels {
+					Yes, Changes := CompareChannel(LCh, NCh)
+				}
+			}
+		}
+	}
+	ChangeString = append(ChangeString, NewString)
+}
+
+func StartCheckLoop() {
+	GLDB, err := ioutil.ReadFile("PrimeGuild")
+	if err != nil {
+		fmt.Println("Error reading PG file: " + err.Error())
+		if GLDB == nil {
+			ioutil.WriteFile("PrimeGuild", nil, 9000)
+		}
+		return
+	}
+	PrimeGuild = string(GLDB)
+	ResumeCheck(PrimeGuild)
+}
+
+func Initnewguild(GID string) {
+	fmt.Println("No guild file for '" + "' found!\nCreating new guild files...")
+	BGuild, err := sh.dg.State.Guild(GID)
+	if err == nil {
+		GLD := &GuildInfo{
+			Lastcheck: GetTime(),
+			g:         BGuild,
+			BotUP:     true,
+		}
+		WriteGLDfile(GLD, true)
+		WriteGLDfile(GLD, false)
+	}
+	fmt.Println("New guild file for " + BGuild.Name + " made, ready for logging!")
+}
+
+func ResumeCheck(Gid string) {
+	G := GetGLDfile(Gid)
+	if G.g == nil {
+		Initnewguild(Gid)
+		return
+	}
+	if G.BotUP == true {
+		LastCheck.SiLaChBisTerm = true
+		fmt.Println("Warning: Sherlock hasn't been closed properly since last boot!")
+	} else {
+		LastCheck.SiLaChBisTerm = false
+	}
+	LastCheck.Firstcheck = true
+	NewChange := CheckChange(G, Gid)
+	if LastCheck.Changeyes == true {
+		AppendChange(NewChange)
+	}
+}
+
+type GuildInfo struct {
+	g         *discordgo.Guild
+	Lastcheck struct {
+		Year  int
+		Month *time.Month
+		Day   int
+		Hour  int
+		Min   int
+		Sec   int
+	}
+	BotUP bool
+}
+
+type LastChangeStatus struct {
+	GI            *GuildInfo
+	SiLaChBisTerm bool
+	Firstcheck    bool
+	Changeyes     bool
+	Change        struct {
+		beginvars bool
+		roles     bool
+		members   bool
+		presence  bool
+		channel   bool
+	}
+}
+
+// handlers
+
 func BBReady(s *discordgo.Session, r *discordgo.Ready) {
 	sh.OwnID = r.User.ID
 	sh.OwnAV = r.User.Avatar
 	sh.OwnName = r.User.Username
 	fmt.Println("Discord: Ready message received\nSH: I am '" + sh.OwnName + "'!\nSH: My User ID: " + sh.OwnID)
+
+	StartCheckLoop()
 }
+
+func BBCreateMessage(Ses *discordgo.Session, MesC *discordgo.MessageCreate) {
+	Mes := MesC.Message
+	if Mes.Content[0] == '!' {
+		ProcessCMD(Mes.Content[1:], Mes)
+	}
+}
+
+// misc funx
+
+func ProcessCMD(CMD string, M *discordgo.Message) {
+	if CMD[:8] == "SetPrime" {
+		PID := CMD[9:]
+		data := []byte(PID)
+		ioutil.WriteFile("PrimeGuild", data, 9000)
+		fmt.Println("Set new Prime Guild to '" + PID + "'")
+		sh.dg.ChannelMessageSend(M.ChannelID, "`Set new prime guild to "+PID+"`")
+	}
+}
+
+func GetTime() (int, *time.Month, int, int, int, int) {
+	Year := time.Now().Year()
+	Month := time.Now().Month()
+	Day := time.Now().Day()
+	Hour := time.Now().Hour()
+	Min := time.Now().Minute()
+	Sec := time.Now().Second()
+}
+
+func GetGLDfile(GID string) GuildInfo {
+	DATA, err := ioutil.ReadFile(GID + ".GLD")
+	if err == nil {
+		var LLG GuildInfo
+		err := json.Unmarshal(DATA, LLG)
+		if err == nil {
+			return LLG
+		} else {
+			fmt.Println("Error Unmarshal-ing GLD: " + err.Error())
+		}
+	}
+	var Errorstring string
+	var NoGFile bool
+	if err != nil {
+		Errorstring = err.Error()
+		NoGFile = strings.ContainsAny(GID, Errorstring)
+	} else {
+		NoGFile = false
+	}
+	if err != nil {
+		if NoGFile == true {
+			return GuildInfo{}
+		} else {
+			fmt.Println("Error reading GLD file: " + Errorstring)
+		}
+	}
+	return GuildInfo{}
+}
+
+func WriteGLDfile(G *GuildInfo, Isb bool) error {
+	GID, GIDerr := json.Marshal(G)
+	if GIDerr == nil {
+		if Isb == false {
+			ioutil.WriteFile(G.g.ID+".GLD", GID, 0777)
+		}
+		if Isb == true {
+			ioutil.WriteFile("B-"+G.g.ID+".GLD", GID, 0777)
+		}
+		return nil
+	} else {
+		fmt.Println("GLD writing error: " + GIDerr.Error())
+		return GIDerr
+	}
+}
+
+// init
 
 func Initialize(Token string) {
 	isdebug, err := ioutil.ReadFile("debugtoggle")
@@ -53,6 +277,7 @@ func Initialize(Token string) {
 	}
 	// handlers
 	sh.dg.AddHandler(BBReady)
+	sh.dg.AddHandler(BBCreateMessage)
 
 	fmt.Println("SH: Handlers installed")
 
